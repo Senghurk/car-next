ff67ce8fa27dcbcbe301fdd2b935bc4034992320
Senghurk <bin4ryhunt3r@gmail.com>
2024-08-28 11:51:12 +0700

    Initial Commit

--- app/api/vat/route.js
+++ app/api/vat/route.js
@@ -8,3 +8,20 @@ export async function GET(request) {
       vat,
     });
   }
+
+  export async function POST(request) {
+    const data = await request.json();
+    console.log(data);
+  
+    const amount = data.amount
+    const rate = parseFloat(process.env.VAT_RATE);
+    const vat = Math.round(amount * rate) * 100 / 100;
+  
+  
+    return Response.json({
+      rate,
+      amount,
+      vat,
+    });
+  
+  }
