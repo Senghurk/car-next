8b5a501415b2bb3321b31b684478c408218c6595
Senghurk <bin4ryhunt3r@gmail.com>
2024-08-28 10:59:30 +0700

    Initial Commit

--- app/vat/page.js
+++ app/vat/page.js
@@ -1,26 +1,38 @@
-import Image from "next/image";
+"use client"
+import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+    const { register, handleSubmit } = useForm();
+    const [vat, setVat] = React.useState(0);
+    const handleForm = (data) => {
+        console.debug(data)
+        const v = Math.round(data.price * data.rate * 100) / 100
+        setVat(v)
+    }
     return (
         <div>
             <h1>VAT Calcuator</h1>
-            <form>
+            <form onSubmit={handleSubmit(handleForm)}>
                 <table>
                     <tbody>
                         <tr>
                             <td><label>Price:</label></td>
-                            <td><input type="text" name="price" /></td>
+                            <td><input type="number" name="price" 
+                            {...register("price",{valueAsNumber:true})}/></td>
+                        </tr>
+                        <tr>
+                            <td><label>VAT Rate:</label></td>
+                            <td><input type="number" name="rate" value="0.07" readOnly
+                            {...register("rate",{valueAsNumber:true})}/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="2"><input type="submit" value="Calculate"
+                             /></td>
                         </tr>
                         <tr>
                             <td><label>VAT:</label></td>
-                            <td><input type="number" name="vat" value="0.07" readOnly/></td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"><input type="submit" value="Calculate" /></td>
-                        </tr>
-                        <tr>
-                            <td><label>Result:</label></td>
-                            <td><input type="text" name="result" /></td>
+                            <td><input type="text" name="vat" value={vat}/></td>
                         </tr>
                     </tbody>
                 </table>
